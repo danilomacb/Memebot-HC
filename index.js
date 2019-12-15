41cb2ee677068c40a103f71cd9030bfe119c88c3
danilomacb <danilo.bakun@gmail.com>
2019-12-15 17:33:12 -0300

    investigacao

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content === config.prefix + 'ping') {
-        message.channel.send('Pong.');
+    switch(message.content) {
+        case config.prefix + 'ping':
+            message.channel.send('Pong.');
+            break;
+        
+        case config.prefix + 'investigacao':
+            message.channel.send('Em caso de investigação policial, eu declaro que não tenho envolvimento com este grupo e não sei como estou no mesmo, provavelmente fui inserido por terceiros, declaro que estou disposto a colaborar com as investigações e estou disposto a me apresentar a depoimento se necessário.');
+            break;
     }
 });
 
