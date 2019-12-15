a41cbb73e11cabd17b75cdf9e4409fdfa351cf0c
danilomacb <danilo.bakun@gmail.com>
2019-12-15 17:03:23 -0300

    ping pong

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('message', message => {
+	if (message.content === '!ping') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Pong.');
+    }
+});
+
 client.login('NjU1ODU0OTQ4NTMzMjcyNTg2.XfaM4g.ndQ8Zr-2bt_Pcdr7sc3m0bVcnYk');
