145678e6960527386b6e5859d8c1acc4c9bc6612
danilomacb <danilo.bakun@gmail.com>
2019-12-15 16:48:49 -0300

    creating index

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('NjU1ODU0OTQ4NTMzMjcyNTg2.XfaM4g.ndQ8Zr-2bt_Pcdr7sc3m0bVcnYk');
