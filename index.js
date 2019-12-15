147062033750ddbebd0f409c065d950435f035c0
danilomacb <danilo.bakun@gmail.com>
2019-12-15 17:47:59 -0300

    installing prettier

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const Discord = require('discord.js');
-const config = require('./config.json');
+const Discord = require("discord.js");
+const config = require("./config.json");
 
 const client = new Discord.Client();
 
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
-client.on('message', message => {
-    switch(message.content) {
-        case config.prefix + 'ping':
-            message.channel.send('Pong.');
-            break;
-        
-        case config.prefix + 'investigacao':
-            message.channel.send('Em caso de investigação policial, eu declaro que não tenho envolvimento com este grupo e não sei como estou no mesmo, provavelmente fui inserido por terceiros, declaro que estou disposto a colaborar com as investigações e estou disposto a me apresentar a depoimento se necessário.');
-            break;
-    }
+client.on("message", message => {
+  switch (message.content) {
+    case config.prefix + "ping":
+      message.channel.send("Pong.");
+      break;
+
+    case config.prefix + "investigacao":
+      message.channel.send(
+        "Em caso de investigação policial, eu declaro que não tenho envolvimento com este grupo e não sei como estou no mesmo, provavelmente fui inserido por terceiros, declaro que estou disposto a colaborar com as investigações e estou disposto a me apresentar a depoimento se necessário."
+      );
+      break;
+  }
 });
 
 client.login(config.token);
