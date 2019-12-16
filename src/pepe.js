c1e43bd1822291bc98b1d7ea467c976fc6b565c2
danilomacb <danilo.bakun@gmail.com>
2019-12-16 15:32:54 -0300

    adding more pepes

--- src/pepe.js
+++ src/pepe.js
@@ -11,6 +11,54 @@ async function pepe(message) {
     case prefix + "pepehands":
       message.channel.send(new Discord.Attachment("images/pepe/pepehands.png"));
       break;
+
+    case prefix + "pepepopcorn":
+      message.channel.send(new Discord.Attachment("images/pepe/pepepopcorn.png"));
+      break;
+
+    case prefix + "pepethink":
+      message.channel.send(new Discord.Attachment("images/pepe/pepethink.png"));
+      break;
+
+    case prefix + "poggers":
+      message.channel.send(new Discord.Attachment("images/pepe/poggers.png"));
+      break;
+
+    case prefix + "pepehappy":
+      message.channel.send(new Discord.Attachment("images/pepe/pepehappy.png"));
+      break;
+
+    case prefix + "suspepe":
+      message.channel.send(new Discord.Attachment("images/pepe/suspepe.png"));
+      break;
+
+    case prefix + "pepepanties":
+      message.channel.send(new Discord.Attachment("images/pepe/pepepanties.png"));
+      break;
+
+    case prefix + "190":
+      message.channel.send(new Discord.Attachment("images/pepe/190.png"));
+      break;
+
+    case prefix + "monka":
+      message.channel.send(new Discord.Attachment("images/pepe/monka.png"));
+      break;
+
+    case prefix + "triplemonka":
+      message.channel.send(new Discord.Attachment("images/pepe/triplemonka.png"));
+      break;
+
+    case prefix + "yikes":
+      message.channel.send(new Discord.Attachment("images/pepe/yikes.png"));
+      break;
+
+    case prefix + "pepekms":
+      message.channel.send(new Discord.Attachment("images/pepe/pepekms.png"));
+      break;
+
+    case prefix + "pepelove":
+      message.channel.send(new Discord.Attachment("images/pepe/pepelove.png"));
+      break;
   }
 }
 
