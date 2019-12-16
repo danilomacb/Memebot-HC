bb817d984997b5c10f08b5265b4073ea6fce99c7
danilomacb <danilo.bakun@gmail.com>
2019-12-16 16:37:20 -0300

    update help

--- src/text.js
+++ src/text.js
@@ -4,8 +4,22 @@ async function text(message) {
   switch (message.content) {
     case prefix + "help":
       message.channel.send(
-        "```Comandos: \n\n$help\n$investigacao\n$stonks\n$lanso\n$npodese\n$pepe\n$big-pepe\n$pepehands\n$big-pepehands```"
+        "```Índice:\n\n$help\n$help-text\n$help-pepe\n$help-memes\n\nDesenvolvido por Danilo Macedo Bakun```"
       );
+      break;
+
+    case prefix + "help-text":
+      message.channel.send("```Lista de Comandos de Texto: \n\n$investigacao```");
+      break;
+
+    case prefix + "help-pepe":
+      message.channel.send(
+        "```Lista Emotes do Pepe:\n\nObs: Você também pode usar a tag big para fazer os emotes do pepe ficarem maiores.\nex: $big-pepe\n\n$pepe\n$pepehands\n$pepepopcorn\n$pepethin\n$poggers\n$pepehappy\n$suspepe\n$pepepanties\n$pepe190\n$monka\n$triplemonka\n$yikes\n$pepekms\n$pepelove```"
+      );
+      break;
+
+    case prefix + "help-memes":
+      message.channel.send("```Lista de Memes: \n\n$stonks\n$lanso\n$npodese```");
       break;
 
     case prefix + "investigacao":
