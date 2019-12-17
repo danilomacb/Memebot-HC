14f7919b382b59ce1dd77f0ab22bcc94f6805fe9
danilomacb <danilo.bakun@gmail.com>
2019-12-17 11:45:10 -0300

    implementing pepes dynamically

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const Discord = require("discord.js");
 
 const text = require("./text");
 const pepe = require("./pepe");
+const pepeloop = require("./pepeloop");
 const bigPepe = require("./bigPepe");
 const memes = require("./memes");
+const list = require("../list.json");
 
 const client = new Discord.Client();
 
@@ -15,8 +17,9 @@ client.once("ready", () => {
 
 client.on("message", async message => {
   text(message);
-  pepe(message);
+  // pepe(message);
   bigPepe(message);
+  pepeloop(message);
   memes(message);
 });
 
