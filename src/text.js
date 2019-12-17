57d56e945105edbd728f4b69f1afab7f7c132991
danilomacb <danilo.bakun@gmail.com>
2019-12-17 16:35:29 -0300

    adding onaruto

--- src/text.js
+++ src/text.js
@@ -7,7 +7,7 @@ async function text(message) {
       break;
 
     case prefix + "help-text":
-      message.channel.send("```Lista de Comandos de Texto: \n\n$investigacao```");
+      message.channel.send("```Lista de Comandos de Texto: \n\n$investigacao\n$onaruto```");
       break;
 
     case prefix + "help-pepe":
@@ -25,6 +25,11 @@ async function text(message) {
         "Em caso de investigação policial, eu declaro que não tenho envolvimento com este grupo e não sei como estou no mesmo, provavelmente fui inserido por terceiros, declaro que estou disposto a colaborar com as investigações e estou disposto a me apresentar a depoimento se necessário."
       );
       break;
+
+    case prefix + "onaruto":
+      message.channel.send(
+        "O Naruto pode ser um pouco duro às vezes, talvez você não saiba disso, mas o Naruto também cresceu sem pai. Na verdade ele nunca conheceu nenhum de seus pais, e nunca teve nenhum amigo em nossa aldeia. Mesmo assim eu nunca vi ele chorar, ficar zangado ou se dar por vencido, ele está sempre disposto a melhorar, ele quer ser respeitado, é o sonho dele e o Naruto daria a vida por isso sem hesitar. Meu palpite é que ele se cansou de chorar e decidiu fazer alguma coisa a respeito!"
+      );
   }
 }
 
